91504ec3a7b5cc534e528a30db3c2da561301c5b
Vivvie Lin <vvlnote@hotmail.com>
2020-01-13 15:22:28 -0800

    MagicSquare 3x3 starting in position(n/2, n-1)

--- MagicSquare.js
+++ MagicSquare.js
@@ -4,17 +4,20 @@ function constructionMagicSquare(n) {
 	let magicSquare = initializeMatrix(n);
 	// if (n % 2 != 0) {
 	// 	if (n == 3) {
- 		magicSquare = magicSquare3x3(n, magicSquare)
+ 		magicSquare3x3(n, magicSquare);
+ 		magicSquare = initializeMatrix(n);
+ 		console.log(`${"*".repeat(10)}`);
+ 		magicSquare3x3_1(n, magicSquare);
 	// 	} else {
 	// 		oddNumberMagicSquare(n, magicSquare);			
 	// 	}
 	// } else {
 	// 	evenNumberMagicSquare(n, magicSquare);
 	// }
-	printOutMagicSquare(magicSquare);
 }
 
 function magicSquare3x3(n, magicSquare) {
+	//starting point (number 1) at position (0, n/2)
 	let i = 1;
 	let currentRow = 0;
 	let currentCol = 1;
@@ -50,10 +53,49 @@ function magicSquare3x3(n, magicSquare) {
 			}
 			magicSquare[currentRow][currentCol] = i;
 		}
-
 		i += 1;
 	}
-	return magicSquare;
+	printOutMagicSquare(magicSquare);
+}
+
+function magicSquare3x3_1(n, magicSquare) {
+	//starting point (number 1) at position (n/2, n-1)
+	let currentRow = Math.floor(n/2);
+	let currentCol = n-1;
+	let i = 1;
+
+	while (i <= n*n) {
+		if (i == 1) {
+			magicSquare[currentRow][currentCol] = i;
+		} else {
+			let nextRow = currentRow - 1;
+			let nextCol = currentCol + 1;
+
+			if (nextRow >= 0) {
+				if (nextCol < n) {
+					if (magicSquare[nextRow][nextCol] != 0) {
+						currentCol -= 1;
+					} else {
+						currentRow -= 1;
+						currentCol += 1;
+					}
+				} else {
+					currentRow -= 1;
+					currentCol = 0;
+				}
+			} else if (nextRow < 0) {
+				if (nextCol < n) {
+					currentRow = n - 1;
+					currentCol += 1;
+				} else {
+					currentCol -= 1;
+				}
+			}
+			magicSquare[currentRow][currentCol] = i;
+		}
+		i += 1;
+	}
+	printOutMagicSquare(magicSquare);
 }
 
 function initializeMatrix(n){
