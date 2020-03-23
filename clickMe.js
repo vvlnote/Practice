849014f84520dae5ef2d1cb584a7baf407e45f3f
Vivvie Lin <vvlnote@hotmail.com>
2020-03-22 18:15:02 -0700

    fix bug, and add another practice of closure

--- clickMe.js
+++ clickMe.js
@@ -1,22 +1,35 @@
 function clickMe(msg) {
-    console.log(`click me with ${msg}`);
     let count = 0;
-    function clickCount() {
+    return function(){
         count += 1;
+        console.log(`${msg} has been click ${count}`);
     }
-    clickCount();
-    return count;
 }
 
-let  clickMeClosure = (function(msg){
+let  clickMeClosure = (function(){
     let count = 0;
     return function() {
        console.log(`this button has been clicked ${++count} times`);
     }
 })();
 
-clickMeClosure();
-clickMeClosure();
-clickMeClosure();
+// clickMeClosure();
+// clickMeClosure();
+// clickMeClosure();
 
+let click = clickMe(`clickMe button`);
+click();
+click();
+click();
+
+let warnUser = function(msg) {
+    let calledCount = 0;
+    return function() {
+        calledCount += 1;
+        console.log(`${msg} you have been warned ${calledCount} times`);
+    }
+}
+let warnForTamper = warnUser(`Hello!`);
+warnForTamper();
+warnForTamper();
 
