ee7bb3727e151e152337b817c27f6b261f12f288
Vivvie Lin <vvlnote@hotmail.com>
2020-03-27 17:06:57 -0700

    convert a function with 4 input parameters to a particail function

--- implementAdd.js
+++ implementAdd.js
@@ -11,8 +11,21 @@ function add4_curry(a) {
         }
     }
 }
+let result = add4(1, 2, 3, 4);
+console.log(`result of function with multiple of arguments = ${result}`);
 // let result = add4_curry(1)(2)(3)(4);
-let result = add4_curry(1)(2);
-console.log(`result = ${result}`);
+ result = add4_curry(1)(2);
+console.log(`result of currying function = ${result}`);
 let final = result(3)(4);
 console.log(`final = ${final}`);
+
+
+function add4_partialFun(a) {
+    return function add3(b,c,d){
+        return a+b+c+d;
+    }
+}
+
+const add1 = add4_partialFun(1);
+result = add1(2,3,4);
+console.log(`result of partial function = ${result}`);
