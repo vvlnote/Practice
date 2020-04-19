86b27eca1cb090a1f7863caa7264b277834dc908
Vivvie Lin <vvlnote@hotmail.com>
2020-04-18 19:27:10 -0700

    112. Path Sum

---
+++ LeetCode/hasPathSum.js
@@ -0,0 +1,10 @@
+var hasPathSum = function(root, sum) {
+
+    if (root === null) return false;
+    if (root.left === null && root.right === null) {
+        return (sum === root.val) 
+    } else {
+        return (hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val));
+
+     }
+};
