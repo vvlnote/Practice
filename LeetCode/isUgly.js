a7a526f0d4ccbc7c8c817fc079d3fd0efddab98a
Vivvie Lin <vvlnote@hotmail.com>
2020-05-06 12:14:01 -0700

    263. Ugly Number - more structured

--- LeetCode/isUgly.js
+++ LeetCode/isUgly.js
@@ -3,18 +3,38 @@
  * @return {boolean}
  */
 var isUgly = function(num) {
-    if (num < 0) return false;
-    while (num % 2 === 0 && num > 1) {
-        num = num / 2;
-    }
+//more structured
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+ if (num <= 0) return false;
+ while (num > 1) {
+     if (num%2 === 0) {
+         num = num /2;
+     } else if (num % 3 === 0) {
+         num = num / 3;
+     } else if (num % 5 === 0) {
+         num = num /5;
+     } else {
+         return false;
+     }
+ }
+ return true;
+
+    // if (num < 0) return false;
+    // while (num % 2 === 0 && num > 1) {
+    //     num = num / 2;
+    // }
     
-    while (num % 3 === 0 && num > 1) {
-        num = num /3;
-    }
-    while (num % 5 === 0 && num > 1) {
-        num = num / 5;
-    }
+    // while (num % 3 === 0 && num > 1) {
+    //     num = num /3;
+    // }
+    // while (num % 5 === 0 && num > 1) {
+    //     num = num / 5;
+    // }
     
-    if (num === 1) return true;
-    return false;
+    // if (num === 1) return true;
+    // return false;
 };
