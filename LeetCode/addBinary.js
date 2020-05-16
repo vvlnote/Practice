9a66073a4202dcc8e1f461e56a5ae931dc3e6ca9
Vivvie Lin <vvlnote@hotmail.com>
2020-05-16 16:00:15 -0700

    67. Add Binary - more structured way to implement it

--- LeetCode/addBinary.js
+++ LeetCode/addBinary.js
@@ -3,6 +3,33 @@
  * @param {string} b
  * @return {string}
  */
+// more compact method. without reversing the strings. 
+// 1. make both string are same size.
+// 2. sum = a + b + carry 
+// 3. the result of sum % 2 will be the integer of i position in the ans 
+// 4. the result of Math.floor (sum/2) will be assigned to carry
+
+var addBinary = function(a, b) {
+ let temp = a.length - b.length;
+ if (temp > 0) {
+     b = b.padStart(a.length, "0");
+ }
+ if (temp < 0) {
+     a = a.padStart(b.length, '0');
+ }
+ let ans = Array(a.length).fill('0');
+ let carry = 0;
+ for (let i = a.length - 1; i >= 0; i--) {
+     let sum = parseInt(a[i]) + parseInt(b[i]) + carry; 
+     ans[i] = sum%2;
+     carry = Math.floor(sum/2);
+ }
+ if (carry === 1) {
+     ans.unshift('1');
+ }
+ return ans.join('');
+}
+ /* first try
 var addBinary = function(a, b) {
     let aArr = a.split('').reverse();
     let bArr = b.split('').reverse();
@@ -65,3 +92,4 @@ var addBinary = function(a, b) {
     }
     return ans.reverse().join('');
 };
+*/
