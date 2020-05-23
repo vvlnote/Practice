f8cb03e4255e45a80e1e4138d80de22223f54c0d
Vivvie Lin <vvlnote@hotmail.com>
2020-05-22 17:36:47 -0700

    16. 3Sum Closest - use 2 pointers to implement, Time Complexity is O(n^2), space Complexity is O(1)

--- LeetCode/threeSumClosest.js
+++ LeetCode/threeSumClosest.js
@@ -4,20 +4,26 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-    let diffArr = [];
-    let minDiff = Number.MAX_VALUE;
-    let minNum = 0;
-    for (let i = 0; i < nums.length -2; i++) {
-        for (let j = i + 1; j < nums.length - 1; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                let sum = nums[i] + nums[j] + nums[k];
-                // console.log(sum);
-                if (minDiff > Math.abs(target - sum)) {
-                    minDiff = Math.abs(target-sum);
-                    minNum = sum;
-                }
-            }
-        }
-    }
-    return minNum;
+ let minDiff = Number.MAX_VALUE;
+ let minNum = 0;
+ nums = nums.sort((a, b) => a - b);
+ for (let i = 0; i < nums.length -2; i ++) {
+     let j = i + 1;
+     let k = nums.length -1;
+     while (j < k) {
+         let sum = nums[i] + nums[j] + nums[k];
+         if (minDiff > Math.abs(sum - target)) {
+             minDiff = Math.abs(sum- target);
+             minNum = sum;
+         }
+         if (sum < target) {
+             j += 1;
+         } else if (sum > target) {
+             k -= 1;
+         } else {
+             return sum;
+         }
+     }
+ }
+ return minNum;
 };
