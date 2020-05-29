fdb7c6d0c634ce8b01f32c132aee01a4b8689b39
Vivvie Lin <vvlnote@hotmail.com>
2020-05-28 17:34:44 -0700

    896. Monotonic Array, 2nd implemention, reduce the comparison to speed up the running time

--- LeetCode/isMonotonic.js
+++ LeetCode/isMonotonic.js
@@ -4,19 +4,34 @@
  */
 var isMonotonic = function(A) {
     let i = 0;
-    function isMontone(arr, startingIndex, increase) {
-        for (let i = startingIndex; i < arr.length - 1; i ++) {
-            // console.log(`A[i] = ${A[i]}, A[i+1] = ${A[i+1]}`);
-            if (increase) {
-                if (A[i] > A[i+1]) return false;
-            }
-            if (!increase) {
-                if (A[i] < A[i+1]) return false;
-            }
-        }
-        return true;
-    }
+    // function isMontone(arr, startingIndex, increase) {
+    //     for (let i = startingIndex; i < arr.length - 1; i ++) {
+    //         // console.log(`A[i] = ${A[i]}, A[i+1] = ${A[i+1]}`);
+    //         if (increase) {
+    //             if (A[i] > A[i+1]) return false;
+    //         }
+    //         if (!increase) {
+    //             if (A[i] < A[i+1]) return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     
+
+ function isMontone(arr, startingIndex, increase) {
+     if (increase) {
+         for (let i = startingIndex; i < arr.length -1; i ++) {
+             if (A[i] > A[i+1]) return false;
+         }
+         return true;
+     }
+     if (!increase) {
+         for (let i = startingIndex; i < arr.length -1; i ++) {
+             if (A[i] < A[i+1]) return false;
+         }
+         return true;            
+     }
+ };
     while(i < A.length - 1) {
         if (A[i] < A[i+1]) {
             return isMontone(A, i + 1, true);
